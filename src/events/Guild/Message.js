02dce12823d40e8f054156f7c06e3494e2b917d6
{Bubbly} <68120990+BubblyDisc@users.noreply.github.com>
2021-07-11 17:25:41 +0000

    Stopped a error if nothing in database.

--- src/events/Guild/Message.js
+++ src/events/Guild/Message.js
@@ -1,49 +1,74 @@
-const { MessageEmbed } = require('discord.js')
-const fetch = require('node-fetch')
-const ms = require('ms')
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const ms = require("ms");
 
 module.exports = {
-    name: 'message',
-    run: async (client, message) => {
-        if (!message.guild || message.author.bot) return;
+  name: "message",
+  run: async (client, message) => {
+    if (!message.guild || message.author.bot) return;
 
-        fetch(`http://localhost:4002/guilds/${message.guild.id}/data`)
-            .then(res => res.json())
-            .then(async json => {
-                const dataPrefix = json.Preifx
+    fetch(`http://localhost:4002/guilds/${message.guild.id}/data`)
+      .then((res) => res.json())
+      .then(async (json) => {
+        let dataPrefix;
 
-                dataPrefix.push('<@!763420472666357811>')
+        if (json.Preifx) {
+          dataPrefix = json.Preifx;
+        } else {
+          dataPrefix = ["b!"];
+        }
 
-                const prefix = dataPrefix.find(p => message.content.toLowerCase().startsWith(p))
+        dataPrefix.push("<@!763420472666357811>");
 
-                if (!prefix) return;
+        const prefix = dataPrefix.find((p) =>
+          message.content.toLowerCase().startsWith(p)
+        );
 
-                const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
-                const command = await client.commands.get(cmd.toLowerCase()) ?? client.commands.get(client.aliases.get(cmd.toLowerCase()))
+        if (!prefix) return;
 
-                if (!command) return;
+        const [cmd, ...args] = message.content
+          .slice(prefix.length)
+          .trim()
+          .split(/ +/g);
+        const command =
+          (await client.commands.get(cmd.toLowerCase())) ??
+          client.commands.get(client.aliases.get(cmd.toLowerCase()));
 
-                if (command) {
+        if (!command) return;
 
-                    if (command.ownerOnly == true) {
-                        if (!client.owners.includes(message.author.id)) return;
-                    }
+        if (command) {
+          if (command.ownerOnly == true) {
+            if (!client.owners.includes(message.author.id)) return;
+          }
 
-                    if (client.cooldowns.has(`${message.author.id}-${command.name}`)) {
-                        return message.channel.send(`Dude Chill ${message.author}, Could you try runing this command in \`${ms(client.cooldowns.get(`${message.author.id}-${command.name}`) - Date.now(), { long: true })}\``)
-                    }
+          if (client.cooldowns.has(`${message.author.id}-${command.name}`)) {
+            return message.channel.send(
+              `Dude Chill ${
+                message.author
+              }, Could you try runing this command in \`${ms(
+                client.cooldowns.get(`${message.author.id}-${command.name}`) -
+                  Date.now(),
+                { long: true }
+              )}\``
+            );
+          }
 
-                    if (command.cooldown) {
-                        client.cooldowns.set(`${message.author.id}-${command.name}`, Date.now() + command.cooldown)
+          if (command.cooldown) {
+            client.cooldowns.set(
+              `${message.author.id}-${command.name}`,
+              Date.now() + command.cooldown
+            );
 
-                        setTimeout(() => {
-                            client.cooldowns.delete(`${message.author.id}-${command.name}`)
-                        }, command.cooldown)
-                    }
-                    try {
-                        command.run(client, message, args)
-                    } catch { return; }
-                }
-            });
-    }
-}
+            setTimeout(() => {
+              client.cooldowns.delete(`${message.author.id}-${command.name}`);
+            }, command.cooldown);
+          }
+          try {
+            command.run(client, message, args);
+          } catch {
+            return;
+          }
+        }
+      });
+  },
+};
