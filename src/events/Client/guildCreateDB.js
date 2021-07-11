40d02c49d53c31400b13297809973dd8eceb6aa1
{Bubbly} <68120990+BubblyDisc@users.noreply.github.com>
2021-07-11 17:21:21 +0000

    Removed Thank's for inviting me! ❤ embed when joining the guild
    No need for it.

--- src/events/Client/guildCreateDB.js
+++ src/events/Client/guildCreateDB.js
@@ -30,25 +30,5 @@ module.exports = {
 
         const guildClient = new WebhookClient('863828246801154118', 'OBMtIJWu9YfwbNvT1c1Q0HIkZf4alca9yyicrJL0j11HgtAwtluzBl7UvoPlymmRrOZu');
         guildClient.send(`> New Guild!\n > Name: ${guild.name}\n > ID: ${guild.id}\n > Members: ${guild.memberCount}\n > Now in ${client.guilds.cache.size} guilds`)
-        
-        let channelSend;
-
-        guild.channels.cache.forEach((channel) => {
-            if (channel.type == "text") {
-                if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-                    channelSend = channel;
-                }
-            }
-        })
-
-        if (!channelSend) return;
-
-        channelSend.send(
-            new MessageEmbed()
-                .setTitle(`Thank's for inviting me! ❤`)
-                .setDescription(`My prefix is \`b!\`And You can see my commands by runing the command \`b!help\`.`)
-                .setColor('e45962')
-                .setTimestamp()
-        )
     }
 }
